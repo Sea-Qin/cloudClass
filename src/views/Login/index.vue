73d0aef2c8dc85e97e09c8171c8328cd9b3f68a7
Sea-Qin <1545869318@qq.com>
2020-11-05 16:57:33 +0800

    login page update

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -7,56 +7,78 @@
     <!-- <div class="from"> -->
     <div class="login-wrapper">
       <el-tabs v-model="activeName">
-        <el-tab-pane label="登录" name="login"> <!-- 登录-->
+        <el-tab-pane label="登录" name="login">
+          <!-- 登录表单-->
           <el-form
-            :model="logiData"
+            :model="logiForm"
+            status-icon
             :rules="rules"
-            ref="ruleForm"
+            ref="logiForm"
             class="demo-ruleForm"
-            size="mini"
           >
             <el-form-item prop="name">
-              <el-input v-model="logiData.name"
-                placeholder="请输入昵称"></el-input>
+              <el-input
+                v-model="logiForm.name"
+                prefix-icon="el-icon-user-solid"
+                class="form-item"
+                size="mini"
+                placeholder="请输入你的账号昵称"></el-input>
             </el-form-item>
             <el-form-item prop="password">
-              <el-input v-model="logiData.password"
-                placeholder="请输入密码"
-                show-password autocomplete="off"
-              ></el-input>
+              <el-input
+                v-model="logiForm.password"
+                prefix-icon="el-icon-lock"
+                class="form-item"
+                size="mini"
+                placeholder="请输入你的密码"></el-input>
             </el-form-item>
             <el-form-item>
               <el-button
-                type="primary"
-                @click="submitForm('ruleForm')"
+                @click="submitLogiForm('logiForm')"
+                 class="form-item"
               >登录</el-button>
             </el-form-item>
+            <p class="tips">若未注册账户，请先注册！</p>
           </el-form>
         </el-tab-pane>
-        <el-tab-pane label="注册" name="register"> <!-- 注册-->
+        <el-tab-pane label="注册" name="register">
+          <!-- 注册表单-->
           <el-form
-            :model="regiData"
+            :model="regiForm"
+            status-icon
             :rules="rules"
-            ref="ruleForm"
+            ref="regiForm"
             class="demo-ruleForm"
-            size="mini"
           >
             <el-form-item prop="name">
-              <el-input v-model="regiData.name"
-                placeholder="请输入昵称"></el-input>
+              <el-input
+                v-model="regiForm.name"
+                prefix-icon="el-icon-user-solid"
+                class="form-item"
+                size="mini"
+                placeholder="请输入2到8个字符的昵称"></el-input>
             </el-form-item>
             <el-form-item prop="password">
-              <el-input v-model="regiData.password"
-                placeholder="请输入密码"
-                show-password autocomplete="off"
-              ></el-input>
+              <el-input
+                v-model="regiForm.password"
+                prefix-icon="el-icon-lock"
+                class="form-item"
+                size="mini"
+                placeholder="请输入6到15个字符的密码"></el-input>
+            </el-form-item>
+            <el-form-item prop="identify" id="identify">
+              <el-radio-group v-model="regiForm.identify">
+                <el-radio label="0">教师</el-radio>
+                <el-radio label="1">学生</el-radio>
+              </el-radio-group>
             </el-form-item>
             <el-form-item>
               <el-button
-                type="success"
-                @click="submitForm('ruleForm')"
+                @click="submitRegiForm('regiForm')"
+                class="regiBtn"
               >注册</el-button>
             </el-form-item>
+            <p class="tips">请注意确认你的账号身份！</p>
           </el-form>
         </el-tab-pane>
       </el-tabs>
@@ -65,18 +87,20 @@
 </template>
 
 <script>
+/* eslint-disable */
 export default {
   name: 'login',
   data() {
     return {
-      activeName: 'login',
-      logiData: {
+      activeName: 'login', // tab
+      logiForm: {
         name: '',
         password: ''
       },
-      regiData: {
+      regiForm: {
         name: '',
-        password: ''
+        password: '',
+        identify: "1"
       },
       rules: {
         name: [
@@ -99,51 +123,85 @@ export default {
     };
   },
   methods: {
-    submitForm(formName) {
+    submitRegiForm(formName) { // formName为ref参数值
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          // alert('submit!');
-          return true;
+          console.log('注册', this.regiForm);
+          // this.isError = !this.isError;
+          // return true;
         }
-        return false;
+      });
+    },
+    submitLogiForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          console.log('登录', this.logiForm);
+          // this.isError = !this.isError;
+          // return true;
+          this.$router.push('/main');
+        }
       });
     }
+    // handleClickTab(tab, event) {
+    //   this.$message(tab, event);
+    // }
   }
 };
 </script>
 
 <style lang="scss">
-.login-wrapper .is-top{
+@import '@/theme/variable.scss';
+// tab调整
+.is-top {
   font-size: 0.1rem;
 }
-.el-input__inner {
-  border: none;
-  font-size: 0.08rem;
-  // height: 0.20rem;
-  // line-height: 0.20rem;
-  // font-weight: normal;
-  // margin: 0;
-  // width: 1.24rem;
-  // width: 1.5rem;
+.el-tabs__header {
+  margin-bottom: 5px;
+}
+.el-tabs__nav {
+height: 34px;
 }
 .el-form-item {
-    margin-bottom: 0.1rem;
-}
-.el-form-item__label {
-  // margin-right: 3px;
-  font-size: 0.1rem;
-  color: #000;
+  margin-bottom: 5px;
 }
 .el-button {
-  line-height: 0.05rem;
-  font-size: 0.09rem;
-  padding: 8px 60px;
-  margin-top: 25px;
-  // background-color: #fff;
-  // color: #000;
+  font-size: 0.1rem;
+  width: 100%;
+  padding: 8px 0;
+  border: 1px solid #7986bd;
+  color: #fff;
+  border-radius: 40px;
+  background: linear-gradient(90deg, #0162c8, #55e7fc);
+  margin-top: 30px;
 }
-.is-error {
-  font-size: 0.05rem;
+.el-button:hover {
+  // 不设置
+  background: linear-gradient(90deg, #0162c8, #55e7fc);
+  color: #fff;
+  font-size: 0.12rem;
+}
+.el-form-item__error {
+  font-size: 0.08rem;
+  padding: 0;
+}
+.el-input__inner {
+  font-size: 0.08rem;
+  // padding: 2px;
+}
+#identify {
+  padding: 0;
+  margin: 0;
+  margin-top: -8px;
+  height: 30px;
+}
+.el-radio__label {
+  font-size: 0.1rem;
+}
+.el-radio__input {
+  width: 0.1rem;
+  height: 0.14rem;
+  // border: 1px solid;
+  // line-height: 0.1rem;
 }
 </style>
 <style lang="scss" scoped>
@@ -170,11 +228,27 @@ export default {
 }
 .login-wrapper {
   width: 140px;
+  height: 210px;
   padding: 0 25px;
   // height: 200px;
-  border-radius: 4px;
-  background-color: rgb(11, 134, 143);
+  border-radius: 2px;
+  background-color: #fff;
   margin: 15px auto;
   text-align: center;
+  overflow: hidden;
+}
+.regiBtn {
+  margin-top: 0;
+  background: linear-gradient(90deg, #755bea, #ff72c0);
+}
+.regiBtn:hover {
+  background: linear-gradient(90deg, #755bea, #ff72c0);
+  color: #fff;
+  font-size: 0.12rem;
+}
+.tips {
+  font-size: 0.07rem;
+  color: $--color-primary;
+  text-decoration: underline;
 }
 </style>
